5267cb6ec5933a067fcc9e87b99754b86256ee40
AnnaHrulkova <annahrulkova@yandex.ru>
2025-06-20 18:49:55 +0300

    модуль для яндекса

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TranslateService } from '../translate/translate.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService, private readonly translateService: TranslateService) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+    @Get('translate')
+  async translate(@Body('text') text: string) {
+    const translated = await this.translateService.translateByYandex(text);
+    return { translated };
   }
 }
